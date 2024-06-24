9ccf801046573e2f12bfa41275d98fa2b6df40f5
Jn-html <marmainjean@gmail.com>
2024-06-24 19:49:57 +0200

    Change in commentary

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// In-Game variables ####
+// Initialise In-Game variables ####
 // ######################
 let xp = 0;
 let health = 100;
 let gold = 50;
 currentWeaponIndex = 0;
-// fighting use for monster index
+// fighting used for monster index
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
