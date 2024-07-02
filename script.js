2271f4bf8f96fe21151c8a4d82806b354e54399f
Jn-html <marmainjean@gmail.com>
2024-07-02 16:49:07 +0200

    update buyHealth function to check if player has enough gold

--- script.js
+++ script.js
@@ -80,11 +80,15 @@ function fightDragon() {
 }
 
 function buyHealth() {
-    gold -= 10;
-    health += 10;
-    // display health and gold variables on the game screen
-    goldText.innerText = gold
-    healthText.innerText = health
+    if (gold >= 10) {
+        gold -= 10;
+        health += 10;
+        // display health and gold variables on the game screen
+        goldText.innerText = gold;
+        healthText.innerText = health;
+      } else {
+        text.innerText = "You do not have enough gold to buy health."
+      }
 }
 
 function buyWeapon() {
