f708ebe93c178d60f8e252e70413bc3dd8bf3f5b
Jn-html <marmainjean@gmail.com>
2024-07-06 21:53:43 +0200

    Add a location in locations(array) for fight 'screen'

--- script.js
+++ script.js
@@ -81,6 +81,12 @@ const locations = [
 		"button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
 		"button functions": [fightSlime, fightBeast, goTown],
 		text: "You enter the cave. You see some monsters." 
+	},
+	{
+		name: "fight",
+		"button text": ["Attack", "Dodge", "Run"],
+		"button functions": [attack, dodge, goTown],
+		text: "You are fighting a monster." 
 	}
 ];
 
@@ -190,5 +196,5 @@ function attack() {
 }
 
 function dodge() {
-	
+
 }
