84cc57fc8c2d3b34965869d472a5ee6428726d2a
Jn-html <marmainjean@gmail.com>
2024-07-09 16:55:30 +0200

    Add gold & xp + show gold & xp + update location screen

--- script.js
+++ script.js
@@ -222,5 +222,9 @@ function lose() {
 }
 
 function defeatMonster() {
-
+	gold += Math.floor(monsters[fighting].level * 6.7);
+	xp += monsters[fighting].level;
+	goldText.innerText = gold;
+  xpText.innerText = xp;
+	update(locations[4]);
 }
